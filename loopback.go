00bcc563748f0fd9964400e6f4752987153f92bf
Kevin Paul Herbert <kph@platinasystems.com>
2019-04-30 12:28:17 -0700

    loopback: Initial commit

---
+++ loopback.go
@@ -0,0 +1,38 @@
+// Copyright © 2019 Platina Systems, Inc. All rights reserved.
+// Use of this source code is governed by the GPL-2 license described in the
+// LICENSE file.
+
+// Package loopback provides io.Writer and io.Reader compatible interfaces
+// that loop back output to input.
+package loopback
+
+import (
+	"errors"
+	"io"
+)
+
+// Loopback holds the pending loopback data
+type Loopback struct {
+	b []byte // Bytes buffered for I/O
+}
+
+// NotImplemented is the error returned for methods or options
+// that are not implemented.
+var NotImplemented = errors.New("Unimplemented")
+
+// New creates a new loopback interface.
+func New() (l *Loopback) {
+	l = &Loopback{b: make([]byte, 0)}
+	return
+}
+
+// Read returns data which has been previous written to the loopback.
+func (l *Loopback) Read(p []byte) (n int, err error) {
+	return 0, io.EOF
+}
+
+// Write is used to provide data to be looped back for later read
+// operations.
+func (l *Loopback) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
